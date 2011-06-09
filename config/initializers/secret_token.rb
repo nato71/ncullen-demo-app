9cc4ef6a7343d42b001d8b5b7083a4f865293a20
Nathan Cullen <nathan.cullen@weblaunchpros.com>
2011-06-08 21:26:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NcullenDemoApp::Application.config.secret_token = '62577f88d33ca50b56006a61c5efa96e4df50e8377d21edc4b147a45729f5afab16b2a7edb47fde46c9bc48e755af438a5f2e5429b2d6a5c89c424ea443ecace'
